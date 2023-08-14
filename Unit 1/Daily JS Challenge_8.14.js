7ed63772eab0e34c4d661a6b9f266dabb308c3b2
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-14 13:03:42 -0700

    Create Daily JS Challenge_8.14.js

---
+++ Unit 1/Daily JS Challenge_8.14.js
@@ -0,0 +1,59 @@
+/*
+Convert the following if statements into a ternary statement. Create a separate Google doc number and format the if statements and create the ternary in RED and the if statements in BLACK:
+*/
+
+if (paygrade == 7)
+{if (level >= 0 && level <= 8)
+salary *= 1.05;
+else
+salary *= 1.04;} 
+else
+salary *= 1.06;
+
+
+if (value > 0) 
+{ ++increase; }
+else if (value == 0) {++break_even;}
+ else {++decrease;}
+
+
+
+for (i=0; i<sizeof(text); i++)
+ {
+
+switch (text[i]) 
+     {
+case 'A':
+capa++;
+case 'a':
+lettera++;
+default:
+total++;
+} 
+ }
+
+
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+
