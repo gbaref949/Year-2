6768747b98e61c7fa5d08d97bc1e971704588814
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-21 14:00:29 -0700

    Update 10-fs-sync.js

--- Unit 1/NodeJS/01-node-tutorial/10-fs-sync.js
+++ Unit 1/NodeJS/01-node-tutorial/10-fs-sync.js
@@ -1,2 +1,7 @@
 const {readFileSync, writeFileSync} = require('fs')
+const path = require('path');
 console.log('it HAD TO HAPPEN')
+const first = readFileSync(path.join(__dirname, '/content/first.txt'), 'utf8')
+const second = readFileSync(path.join(__dirname, '/content/second.txt'), 'utf8')
+
+console.log(first)
