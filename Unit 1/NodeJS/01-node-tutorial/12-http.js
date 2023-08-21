10fc091b8eb5cf497a1daa3a2ef8a110577c51bf
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-21 15:18:51 -0700

    Update 12-http.js

--- Unit 1/NodeJS/01-node-tutorial/12-http.js
+++ Unit 1/NodeJS/01-node-tutorial/12-http.js
@@ -1,5 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    
+  //Conditional Routing 
+  if(req.url === '/'){
+    res.end('Welcome to the Home page');
+  }else if(req.url === '/about'){
+    res.end('Welcome to the About page')
+  }else{
+    res.end('404 YOU SUCK')
+    res.end(`<h1> Oops! SOmething went wrong </h1>
+    <p>We can't seem to find the page you are looking for.</p>]
+    <a href="/">Home</a>`)
+  }
 })
+console.log("It's running")
+server.listen(5000)
