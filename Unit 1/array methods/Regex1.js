cdae9a2a4ffb502ae8452424cd20adfa32d47b46
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-23 22:01:31 -0700

    Create Regex1.js

---
+++ Unit 1/array methods/Regex1.js
@@ -0,0 +1,28 @@
+/*
+Question # :
+Task:
+
+Text:
+
+Capture Group:
+
+Question #1:
+Task:
+Create a regex that finds dates in the format MM/DD/YY or MM/DD/YYYY and returns just the year part.
+
+Question #2:
+Task:
+Create a regex that finds phone numbers like 602-555-5555 or (602) 555 5555.
+
+Question #3:
+Task:
+Create a regex that returns only the first alphabetic word (upper and lower case) at the start of the string.
+
+Question #4:
+Task:
+Create regex that matches any price in the form of $3.45 or $23.32 or $400.
+
+Question #5:
+Task:
+Construct a regex that captures words that start with a vowel (a, e, i, o, u), can have any number of characters from a-z after the vowel (including zero), and end with a consonant (any letters that are not a, e, i, o, u). For example, it should match “unicorn”, “it”, and “element”.
+*/
