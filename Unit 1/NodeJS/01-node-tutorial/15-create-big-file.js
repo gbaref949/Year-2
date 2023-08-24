b82ec153848feb998e0752619c7fbc7e6f41b092
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-24 13:48:24 -0700

    overwrote file

--- Unit 1/NodeJS/01-node-tutorial/15-create-big-file.js
+++ Unit 1/NodeJS/01-node-tutorial/15-create-big-file.js
@@ -1,9 +1,16 @@
-const {writeFileSync} = require('fs')
-const path = require('path')
+const { writeFileSync } = require('fs');
+const path = require('path');
 //every bit of data is written to the end of the file
 let message = `The interactive Coding prorgam prepares students for a career as a software developer. It teachs students how to desgin and develop softeware and build apps for phones, tablets, website and write and test computer code.${i}\n`;
-for(let i=0; i<1000; i++){
-    writeFileSync(path.join(__dirname, "/content/bigFile.txt"), message, {flag:"a"})
+for (let i = 0; i < 1000; i++) {
+  writeFileSync(path.join(__dirname, '/content/bigFile.txt'), message, {
+    flag: 'a',
+  });
 }
-//10 To Times
-
+//10^5
+//every bit of data is overwrites the last bit of data
+for (let i = 0; i < 1000; i++) {
+  writeFileSync(path.join(__dirname, '/content/bigFile.txt'), message, {
+    flag: 'w',
+  });
+}
