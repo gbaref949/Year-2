fcc7cebd35c3d17ea840a89e686412d3bc45a32f
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-08-29 14:30:00 -0700

    added changes

--- Unit 1/NodeJS/01-node-tutorial/17-http-stream.js
+++ Unit 1/NodeJS/01-node-tutorial/17-http-stream.js
@@ -7,8 +7,16 @@ const PORT = 5000
 
 http.createServer(function(req, res){
     //this is the non streamed so it takes much longer 
-    const text = fs.readFileSync(path.join(__dirname, '/content/bigFile.txt'), 'utf8');
-    res.end()
+    // const text = fs.readFileSync(path.join(__dirname, '/content/newBigFile.txt'), 'utf8');
+    // res.end(text)
+
+    const fileStream = fs.createReadStream(path.join(__dirname, "./content/newBigFile.txt"), 'utf8');
+    fileStream.on("open", () => {
+        fileStream.pipe(res);
+    });
+    fileStream.on("error", (err) => {
+        res.end(err)
+    })
+    
 }).listen(PORT, function(){
-    console.log(`sever listening on ${PORT}`)
-});
+    console.log(`sever listening on ${PORT}`)})
