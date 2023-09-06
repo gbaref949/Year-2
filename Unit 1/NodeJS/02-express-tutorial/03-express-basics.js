c9116619c0229d98be2e0adf8b1cc90fe7890c54
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-09-06 14:34:02 -0700

    updated

---
+++ Unit 1/NodeJS/02-express-tutorial/03-express-basics.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('User Hit Resource')
+    res.status(200).send('Home Page Found')
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page Found')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resources Not Found</h1>')
+})
+
+app.listen(5000, () => {
+    console.log("Listening on http://localhost/5000")})
--- Unit 1/NodeJS/02-express-tutorial/FS Manipulation HW/script.js
+++ Unit 1/NodeJS/02-express-tutorial/FS Manipulation HW/script.js
@@ -11,6 +11,7 @@ const server = http.createServer((req, res) =>{
         res.write('<h1> Home page of Georgie </h1>')
         res.end()
     }
+    //about page
     else if (url === '/about/'){
         res.writeHead(200, {'content-type': 'text/html'})
         fs.copyFile('/index.html', '/about.html', (err) => {
@@ -18,33 +19,52 @@ const server = http.createServer((req, res) =>{
         throw err;
     console.log('source.txt was copied to destination.txt');
         })
-        // res.writeFile(path.join(__dirname, '/about.html'), 'utf8')
+        res.writeFile(path.join(__dirname, '/about.html'), 'utf8')
         res.write('<h1> About page of Georgie </h1>')
         res.end()
     }
-    // else if (url === '/contact/'){
-    //     res.writeHead(200, {'content-type': 'text/html'})
-    //     res.writeFile(path.join(__dirname, '/contact.html'), 'utf8')
-    //     res.write('<h1> About page of Georgie </h1>')
-    //     res.end()
-    // }
-    // else if (url === '/gallery/'){
-    //     res.writeHead(200, {'content-type': 'text/html'})
-    //     res.writeFile(path.join(__dirname, '/gallery.html'), 'utf8')
-    //     res.write('<h1> About page of Georgie </h1>')
-    //     res.end()
-    // }
-    // else if (url === '/testimonials/'){
-    //     res.writeHead(200, {'content-type': 'text/html'})
-    //     res.writeFile(path.join(__dirname, '/testimonials.html'), 'utf8')
-    //     res.write('<h1> About page of Georgie </h1>')
-    //     res.end()
-    // }
+    //contact page
+    else if (url === '/contact/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        fs.copyFile('/index.html', '/contact.html', (err) => {
+    if (err) 
+        throw err;
+    console.log('source.txt was copied to destination.txt');
+        })
+        res.writeFile(path.join(__dirname, '/contact.html'), 'utf8')
+        res.write('<h1> Contact page of Georgie </h1>')
+        res.end()
+    }
+    //gallery page
+    else if (url === '/gallery/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        fs.copyFile('/index.html', '/gallery.html', (err) => {
+    if (err) 
+        throw err;
+    console.log('source.txt was copied to destination.txt');
+        })
+        res.writeFile(path.join(__dirname, '/gallery.html'), 'utf8')
+        res.write('<h1> Gallery page of Georgie </h1>')
+        res.end()
+    }
+    //testimonals page
+    else if (url === '/testimonials/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        fs.copyFile('/index.html', '/testimonials.html', (err) => {
+    if (err) 
+        throw err;
+    console.log('source.txt was copied to destination.txt');
+        })
+        res.writeFile(path.join(__dirname, '/testimonials.html'), 'utf8')
+        res.write('<h1> Testimonials page of Georgie </h1>')
+        res.end()
+    }
+    //404 page
     else{
         res.writeHead(404, {'content-type': 'text/html'})
         res.write('<h1> 404 Not Found </h1>')
         res.end()
     }
-})
+})//server port
 server.listen(5000)
 console.log('Server is running on port')
