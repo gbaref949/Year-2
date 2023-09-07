d82356392958616f516bb4263dc4a40c6f0b3681
G Barefield <gbaref949@west-mec.org>
2023-09-07 11:38:50 -0700

    updated the bellwork

--- Unit 1/array methods/Daily JS Challenge_9.6.js
+++ Unit 1/array methods/Daily JS Challenge_9.6.js
@@ -1,84 +1,87 @@
-/*
-Your flight to the major airline hub reaches cruising altitude without incident. While you consider checking the in-flight menu for one of those drinks that come with a little umbrella, you are interrupted by the kid sitting next to you.
-
-Their handheld game console won't turn on! They ask if you can take a look.
-
-You narrow the problem down to a strange infinite loop in the boot code (your puzzle input) of the device. You should be able to fix it, but first you need to be able to run the code in isolation.
-
-The boot code is represented as a text file with one instruction per line of text. Each instruction consists of an operation (acc, jmp, or nop) and an argument (a signed number like +4 or -20).
-
-acc increases or decreases a single global value called the accumulator by the value given in the argument. For example, acc +7 would increase the accumulator by 7. The accumulator starts at 0. After an acc instruction, the instruction immediately below it is executed next.
-jmp jumps to a new instruction relative to itself. The next instruction to execute is found using the argument as an offset from the jmp instruction; for example, jmp +2 would skip the next instruction, jmp +1 would continue to the instruction immediately below it, and jmp -20 would cause the instruction 20 lines above to be executed next.
-nop stands for No OPeration - it does nothing. The instruction immediately below it is executed next.
-For example, consider the following program:
-
-nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-jmp -4
-acc +6
-These instructions are visited in this order:
-
-nop +0  | 1
-acc +1  | 2, 8(!)
-jmp +4  | 3
-acc +3  | 6
-jmp -3  | 7
-acc -99 |
-acc +1  | 4
-jmp -4  | 5
-acc +6  |
-First, the nop +0 does nothing. Then, the accumulator is increased from 0 to 1 (acc +1) and jmp +4 sets the next instruction to the other acc +1 near the bottom. After it increases the accumulator from 1 to 2, jmp -4 executes, setting the next instruction to the only acc +3. It sets the accumulator to 5, and jmp -3 causes the program to continue back at the first acc +1.
-
-This is an infinite loop: with this sequence of jumps, the program will run forever. The moment the program tries to run any instruction a second time, you know it will never terminate.
-
-Immediately before the program would run an instruction a second time, the value in the accumulator is 5.
-
-Run your copy of the boot code. Immediately before any instruction is executed a second time, what value is in the accumulator?
-*/
 const fs = require('fs');
 
+//I created a fnction to execute the boot code instructions
+function runCode(instructions) {
+  //then I initialized the accumulator and visited set
+  let accumulator = 0;
+  let visited = new Set();
+
+  //created a value to be manipulated with
+  let i = 0;
+
+  while (i < instructions.length) {
+    //then I checked if instruction was already visited if so, we have an infinite loop
+    if (visited.has(i)) {
+      //if the execution failed then return null
+      return null;
+    }
+
+    //then added i to visted
+    visited.add(i);
+
+    //then I split the instruction into operation and argument
+    const [op, arg] = instructions[i].split(' ');
+
+    if (op === 'acc') {
+      //accumulate value
+      accumulator += parseInt(arg);
+    } else if (op === 'jmp') {
+      //this will jump ahead number of instructions based on argument
+      i += parseInt(arg) - 1;
+    }
+
+    //added to i
+    i++;
+  }
+
+  //if the execution succeeds, return accumulator value
+  return accumulator;
+}
+
+//I relaized an error so I tried to fix the code by swapping jmp/nop instructions
+function fixCode(instructions) {
+  for (let i = 0; i < instructions.length; i++) {
+    const [op, arg] = instructions[i].split(' ');
+
+    if (op === 'jmp' || op === 'nop') {
+      //tried swapping jmp and nop instructions
+      const modifiedInstructions = [...instructions];
+      modifiedInstructions[i] = op === 'jmp' ? `nop ${arg}` : `jmp ${arg}`;
+
+      //ran the code with the modified instructions
+      const result = runCode(modifiedInstructions);
+
+      //if the execution succeeds, return accumulator
+      if (result !== null) {
+        return result;
+      }
+    }
+  }
+
+  //returned null
+  return null;
+}
+
+//try catch to implemet the actual code above
 try {
-  //accessed data from input file
-  const data = fs
-    .readFileSync(
-      'Daily JS Challenge_9.6_Input.txt',
-      'utf-8'
-    )
-    const lines = data.split('\n');//split on a new line
-    //console.log(data)  checked data to know where to split 
+  //reads the input file
+  const data = fs.readFileSync(
+    'Unit 1/array methods/Daily JS Challenge_9.6_Input.txt',
+    'utf-8'
+  );
 
-    let regex = /jmp/;
-    let contains = {};
-    
-    function boot(){
-        for(let i = 0; i < data; i++) {
-        let[str, num] = i.split(' ');
-        
-        let acc = 0;
-        for(let j = 0; j < str.length; j++) {
-            if(a === str){
-                acc++;
-            }
-        }
+  //declared a new variable that will slpit the data
+  const instructions = data.split('\n');
 
-        if(regex.test(i)){
-            let check = i.split(' ')[0];
-            contains[check] = true;
-        }else if(nop === NaN){
-            console.log("no operation")
-        }
-    }
-    }
-    
-    console.log(boot[str])
- 
-} 
-catch (err) {
-  //the catch statement will get any errors
-  console.log(err);
+  //declared a new variable that stores instructions
+  const result = fixCode(instructions);
+
+  //console.logged the results
+  if (result !== null) {
+    console.log('Accumulator value on success:', result);
+  } else {
+    console.log('Could not fix code');
+  }
+} catch (err) {
+  console.error(err);
 }
