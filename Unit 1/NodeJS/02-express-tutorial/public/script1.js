ab6a237de67faf42d06972f90dbf82bdf6145b1d
G Barefield <gbaref949@west-mec.org>
2023-09-07 10:26:35 -0700

    Update script1.js

--- Unit 1/NodeJS/02-express-tutorial/public/script1.js
+++ Unit 1/NodeJS/02-express-tutorial/public/script1.js
@@ -1,20 +1,23 @@
-// //use the fs modeule and print out the filelist of ypur desktop
-// const path = require('path');
-// const fs = require('fs');
-// //joining path of directory 
-// const directoryPath = path.join(__dirname, '../../../../../../../../gbaref949/');
-// //passsing directoryPath and callback function
-// fs.readdir(directoryPath, function (err, files) {
-//     //handling error
-//     if (err) {
-//         return console.log('Unable to scan directory: ' + err);
-//     } 
-//     //listing all files using forEach
-//     files.forEach(function (file) {
-//         // Do whatever you want to do with the file
-//         console.log(file); 
-//     });
-// });
-const { path} = require('path'), { http } = require('http'), { fs } = require('fs'), { listen } = 5000, { server }  = http.createServer((res, req) => {
-     console.log('Hello my server')
-})
+//use the fs modeule and print out the filelist of ypur desktop
+const path = require('path');
+const fs = require('fs');
+//joining path of directory
+const directoryPath = path.join(
+  __dirname,
+  '../../../../../../../../gbaref949/'
+);
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+  //handling error
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+    // Do whatever you want to do with the file
+    console.log(file);
+  });
+});
+// const { path} = require('path'), { http } = require('http'), { fs } = require('fs'), { listen } = 5000, { server }  = http.createServer((res, req) => {
+//      console.log('Hello my server')
+// })
