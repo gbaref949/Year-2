323a19b076941c0cf65406bcf28649a683b940c3
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-09-07 14:39:22 -0700

    updated code

--- Unit 1/NodeJS/02-express-tutorial/06-params-query.js
+++ Unit 1/NodeJS/02-express-tutorial/06-params-query.js
@@ -1,7 +1,7 @@
 //Lets go nodemon and postman
 const express = require('express');
 const app = express();
-const {products} = require('./data.js');
+const { products } = require('./data.js');
 
 app.get('/', (req, res) => {
     res.send('<h1> Home Page </h1> <a href="/api/products">Products</a>');
@@ -10,8 +10,8 @@ app.get('/', (req, res) => {
 //Return all products 
 app.get('/api/products', (req, res) => {
     const newProducts = products.map((product) => {
-        const {id, name, age} = product
-        return{id, name, age}
+        const { id, name, age } = product
+        return { id, name, age }
     })
     res.json(newProducts);
 })
@@ -20,17 +20,29 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/products/:productID', (req, res) => {
     console.log(req.params)
-    const {productID} = req.params
+    const { productID } = req.params
     const singleProduct = products.find(
         //You will always get back a number
         (product) => product.id === Number(productID)
     )
-    if(!singleProduct) {
+    if (!singleProduct) {
         return res.status(404).send('Product not found')
     }
     return res.json(singleProduct)
 })
 
+//returns an object that holds all of the params from the url as propertoes
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params)
+    res.send("This product has been reviewed by a person: It's the best there is 10/10 wou;d buy again!")
+})
+
+// //Sets up a query that you can grab
+// app.get('api/v1/query', (req, res) => {
+//     console.log(req.query)
+//     let sortedProducts = [... products]
+// })
+
 app.listen(5000, () => {
     console.log('listening on port 5000');
-  });
+});
