bb56b7fd48e88cb2e4f2a6e313915c23cc9152fd
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-09-11 14:12:52 -0700

    added changes

--- Unit 1/NodeJS/02-express-tutorial/08-middleware-use.js
+++ Unit 1/NodeJS/02-express-tutorial/08-middleware-use.js
@@ -10,6 +10,9 @@ app.use(logger)//moddifiers for the express router
 
 if you have several middle wate then you can call them in an array where order matters
 api/home/products
+
+//API's (https://rapidapi.com/hub)
+//https://documenter.getpostman.com/view/9595116/SW7dWSG1
 */
 
 app.get('/', (req,res)=>{
@@ -20,5 +23,17 @@ app.get('/about', (req,res)=>{
     res.send('About');
 })//will not use the app.use
 
-//API's (https://rapidapi.com/hub)
-//https://documenter.getpostman.com/view/9595116/SW7dWSG1
+app.use('/api', [logger, authorize]);
+/*
+This will aply the logger to any push that include /api as the port of it path
+This is a nice way for you to run a logger and stop a certain amount of req but still allow them on the hom and document 
+Old way of app.use('/api', logger)*/
+
+app.get('/api/products', (req,res)=>{
+    res.send('Products')
+})
+app.get('/api/items', (req,res)=>{
+    console.log(res.user)
+    res.send('Items')
+})
+app.listen(5000, () => {console.log('listening on port 5000')})
