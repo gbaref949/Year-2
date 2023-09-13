643792346578085d61236b8fd8b075faed680480
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-09-13 13:37:12 -0700

    added changes

--- Unit 1/batchFiles/script.js
+++ Unit 1/batchFiles/script.js
@@ -10,50 +10,51 @@ Make sure that the user interface for HTML can point to a folder and navigate to
 Make sure the JS has a string template for renaming
 RETURN THE JS FILE WITH THE CODE
 */
-// Import necessary Node.js modules
-const fs = require('fs'); // File System module for reading and renaming files
-const path = require('path'); // Path module for working with file paths
-const http = require('http'); // HTTP module for creating a server
-const express = require('express'); // Express.js framework for serving web pages
-const bodyParser = require('body-parser'); // Middleware for parsing form data
 
-const app = express(); // Create an Express application
-const port = 3000; // Specify the port for the server to listen on
+//declared and imported the necessary Node.js modules
+const fs = require('fs'); //declared a file System module for reading and renaming files
+const path = require('path'); //declared a path module for working with file paths
+const http = require('http'); //declared a HTTP module for creating a server
+const express = require('express'); //declared a xpress.js framework for serving web pages
+const bodyParser = require('body-parser'); //declared a middleware for parsing form data
 
-// Serve HTML with a file input and customization options when the user accesses the root URL ("/")
+const app = express(); //declared an express app
+const port = 5000; //specified the port to listen on
+
+//created a res for the app to send the request to the home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html')); // Send the HTML file to the client
+  res.sendFile(path.join(__dirname, 'index.html')); //send the html file to the client
 });
 
-app.use(bodyParser.urlencoded({ extended: true })); // Use bodyParser to parse form data
+app.use(bodyParser.urlencoded({ extended: true })); //used bodyParser to parse form data
 
-// Handle file upload and renaming when the user submits the form data to "/upload"
+//this will handle the file upload and renaming when the user submits the form data to "/upload"
 app.post('/upload', (req, res) => {
-  const directoryPath = req.body.folderPath; // Get the folder path from the submitted form data
-  const template = req.body.template; // Get the renaming template from the submitted form data
+  const directoryPath = req.body.folderPath; //got the folder path from the submitted form data
+  const template = req.body.template; //got the renaming template from the submitted form data
 
-  // Read the contents of the specified directory
+  //readdir will read the contents of the specified directory
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
-      return res.send('Unable to scan directory: ' + err); // Handle errors when reading the directory
+      return res.send('Unable to scan directory: ' + err); //then it will handle errors when reading the directory
     }
 
-    // Loop through each file in the directory
+    //used a foreach loop to go through each file in the directory
     files.forEach((file, index) => {
-      const fileExt = path.extname(file); // Get the file's extension (e.g., ".txt", ".jpg")
-      const newName = template.replace('{{index}}', index + 1) + fileExt; // Create the new file name based on the template
-      const oldPath = path.join(directoryPath, file); // Get the old file's path
-      const newPath = path.join(directoryPath, newName); // Get the new file's path
+      const fileExt = path.extname(file); //go the file's extension (e.g., ".txt", ".jpg")
+      const newName = template.replace('{{index}}', index + 1) + fileExt; //created the new file name based on the template
+      const oldPath = path.join(directoryPath, file); //got the old file's path
+      const newPath = path.join(directoryPath, newName); //got the new file's path
 
-      // Rename the file using the new path and name
+      //renamed the file using the new path and name
       fs.renameSync(oldPath, newPath);
     });
 
-    res.send('Files renamed successfully!'); // Send a success message back to the client
+    res.send('Files renamed successfully!'); //used a res to send a success message back to the client
   });
 });
 
-// Create an HTTP server using Express and start listening on the specified port
+//lastly I created an HTTP server to listen on the specified port
 const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
