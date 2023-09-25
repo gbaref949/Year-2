7f48c45d5bde76f379b66e0a6dd43cc50af14ad7
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-09-25 12:14:19 -0700

    finished the edit and delete functions
    I came upon a little bug that I have to fix in class

--- Unit 1/NodeJS/02-express-tutorial/public/script.js
+++ Unit 1/NodeJS/02-express-tutorial/public/script.js
@@ -1,73 +1,106 @@
-const result = document.querySelector(".result")//this will excute on the client side not the browser
-const fetchPeople = async() =>{
-    try{
-        const{data} = await axios.get('/api/people')
-        console.log(data)
-
-        const people = data.data.map((person)=>{ //people is an array of h5 elements filled with their names, jsx
-            return`<h5>${person.name}</h5>`
-        })//data the var, data the array, then map it
-
-        result.innerHTML = people.join("") //joins together all h5 tag with no spaxes
-    }catch(error){
-        console.log(error)
-        formAlert.textContent = error.response.data.msg
-    }
-}
-fetchPeople()//gets the people before it startes creating HTML elememts to fill with those people
-
-const deletePeople = async () =>{
-  try{
-    const{data} = await axios.get(`/api/people/${id}`)
-    console.log(data)
-
-    const {id} = req.params
-    const person = people.find((person) => person.id === Number(id))
-
-    if(!person){
-        return res.status(404).json({success:false, msg: 'No matching ID found'})
-    }
-
-    people = people.filter((person) => {
-        return person.id !== Number(id)
-    })
-    res.status(202).json({data:people, success:true})
-  }catch(error){
-    console.log(error)
-    formAlert.textContent = error.response.data.msg
+const result = document.querySelector('.result'); //this will excute on the client side not the browser
+//function to edit a name
+const editName = async (id, newName) => {
+  try {
+    const { data } = await axios.put(`/api/people/${id}`, { name: newName });
+    fetchPeople();
+    // Put the edited name back into the input box
+    input.value = newName;
+    // Notify the user of the change
+    formAlert.textContent = `Name updated to: ${newName}`;
+  } catch (error) {
+    formAlert.textContent = error.response.data.msg;
   }
-}
-//HTML Submit Form
+};
+
+//function to delete a name
+const deleteName = async (id) => {
+  try {
+    const { data } = await axios.delete(`/api/people/${id}`);
+    fetchPeople();
+  } catch (error) {
+    formAlert.textContent = error.response.data.msg;
+  }
+};
+
+//updated my fetchPeople function to include edit and delete buttons
+const fetchPeople = async () => {
+  try {
+    const { data } = await axios.get('/api/people');
+    console.log(data);
+
+    const people = data.data.map((person) => {
+      return `
+                <div class="person">
+                    <h5>${person.name}</h5>
+                    <button class="edit-btn" data-id="${person.id}">Edit</button>
+                    <button class="delete-btn" data-id="${person.id}">Delete</button>
+                </div>
+            `;
+    });
+
+    result.innerHTML = people.join('');
+
+    // Add event listeners for edit and delete buttons
+    const editButtons = document.querySelectorAll('.edit-btn');
+    const deleteButtons = document.querySelectorAll('.delete-btn');
+
+    editButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const newName = prompt('Enter the new name:');
+        if (newName !== null) {
+          const id = button.getAttribute('data-id');
+          editName(id, newName);
+        }
+      });
+    });
+
+    deleteButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const id = button.getAttribute('data-id');
+        if (confirm('Are you sure you want to delete this name?')) {
+          deleteName(id);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    formAlert.textContent = error.response.data.msg;
+  }
+};
+
+//hTML Submit Form
 const btn = document.querySelector('.submit-btn');
 const input = document.querySelector('.form-input');
 const formAlert = document.querySelector('.form-alert');
 
-btn.addEventListener("click", async (e) => {//if this wasn't here then when you hit submit it would load a blank page
-    e.preventDefault();
-    const nameValue = input.value
+btn.addEventListener('click', async (e) => {
+  //if this wasn't here then when you hit submit it would load a blank page
+  e.preventDefault();
+  const nameValue = input.value;
 
-    try{
-        const {data} = await axios.post("/api/people", {name:nameValue})
-        const h5 = document.createElement("h5")
-        h5.textContent = data.person
-        result.appendChild(h5)
-        fetchPeople()
-    }catch(error){
-        // console.log(error.response)
-        formAlert.textContent = error.response.data.msg;
-    }
-    input.value = ""
-})//prevents default action of submitting and reloading form, we will handle the methods of submit and where it goes
+  try {
+    const { data } = await axios.post('/api/people', { name: nameValue });
+    const h5 = document.createElement('h5');
+    h5.textContent = data.person;
+    result.appendChild(h5);
+    fetchPeople();
+  } catch (error) {
+    // console.log(error.response)
+    formAlert.textContent = error.response.data.msg;
+  }
+  input.value = '';
+}); //prevents default action of submitting and reloading form, we will handle the methods of submit and where it goes
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById('myDropdown').classList.toggle('show');
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.submit-btn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName('dropdown-content');
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
@@ -76,4 +109,4 @@ window.onclick = function(event) {
       }
     }
   }
-}
+};
