f95f30b7f051a071fe6f9abc1e99ce0cd917b518
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-10-03 07:39:44 -0700

    Update task.js

--- Unit 1/NodeJS/02-express-tutorial/cloudTaskManager/models/task.js
+++ Unit 1/NodeJS/02-express-tutorial/cloudTaskManager/models/task.js
@@ -1,22 +1,29 @@
 const moongoose = require('mongoose');
 
-const taskSchema = new moongoose.Schema({
-    FirstName:{
-        type: String,
-        required: [true, 'Must provided a name'],
-        trim:true,
-        maxLength: [20, "The name can be more than 20 characters"]
-
+const taskSchema = new moongoose.Schema(
+  {
+    FirstName: {
+      type: String,
+      required: [true, 'Must provided a name'],
+      trim: true,
+      maxLength: [20, 'The name can be more than 20 characters'],
     },
-    age:{
-        type: Number,
-        default: 5
-    },//comma to contiue to add on to the Schema
-    completed:{
-        type: Boolean,
-        default: false
+    age: {
+      type: Number,
+      default: 21,
     },
-},{collection: "Contacts"})
+    description: String,
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { collection: 'Contacts' }
+);
 
 //module.exports = moongoose.model('Task', TaskSchema)
-module.exports = moongoose.model('Contacts', taskSchema)
+module.exports = moongoose.model('Contacts', taskSchema);
