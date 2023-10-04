f193569d6086d9102a63642e2845d2dc5dd34344
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-10-04 13:14:25 -0700

    switched my api

--- Unit 1/NodeJS/API/api.js
+++ Unit 1/NodeJS/API/api.js
@@ -2,10 +2,14 @@ const axios = require('axios');
 
 const options = {
   method: 'GET',
-  url: 'https://national-weather-service.p.rapidapi.com/zones/%7Btype%7D/%7BzoneId%7D/forecast',
+  url: 'https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D',
+  params: {
+    units: 'auto',
+    lang: 'en'
+  },
   headers: {
     'X-RapidAPI-Key': '494b16716emshea02f4a63fe3d09p1db10ejsn50cfd474da53',
-    'X-RapidAPI-Host': 'national-weather-service.p.rapidapi.com'
+    'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
   }
 };
 
