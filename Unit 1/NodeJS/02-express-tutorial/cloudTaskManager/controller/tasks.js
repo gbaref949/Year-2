97018686f31f8923cebe3a37c8b0f8085df0d485
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-10-11 15:10:13 -0700

    updated the package json

--- Unit 1/NodeJS/02-express-tutorial/cloudTaskManager/controller/tasks.js
+++ Unit 1/NodeJS/02-express-tutorial/cloudTaskManager/controller/tasks.js
@@ -1,9 +1,9 @@
 const { User, Task } = require('../models/task'); //import models
-const { user, task } = require('../data');
+// const { user, task } = require('../data');
 //get tasks and returns them
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find({});
     res.json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
