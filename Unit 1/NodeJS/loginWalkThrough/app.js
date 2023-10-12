aed01b009aa8b1e636efe1fe5717f81066952849
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-10-12 13:49:11 -0700

    first level

--- Unit 1/NodeJS/loginWalkThrough/app.js
+++ Unit 1/NodeJS/loginWalkThrough/app.js
@@ -5,8 +5,14 @@ const port = 5000
 app.set('view engine', 'ejs');//creates a rule that the view engine will be ejs
 //since we are using ejs we need views folder to view anything
 
+const user ={//combines data with templates
+    firstName: 'Tim',
+    lastName: 'Smith',
+}
+
 app.get('/', (req, res) => {
-    res.render('pages/index');//routes at your views folder
+    res.render('pages/index', {user:user});//routes at your views folder
+    //when passing through data through the render
 })
 
 app.listen(port, () => {
